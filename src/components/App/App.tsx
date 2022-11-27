103668ddf686cecf13fe55a2a11003a20f6f21e4
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2022-11-27 23:26:00 +0200

    Add layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import Layout from "../Layout/Layout";
+import Recipes from "../Recipes/Recipes";
+
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Layout name={"Recipes"}>
+                <Recipes/>
+            </Layout>
+        </>
+
     );
 };
 
