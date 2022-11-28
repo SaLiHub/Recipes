90706dc77537a7b727e2950c04ecff411de93c34
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2022-11-29 00:42:55 +0200

    Add Recipes component

---
+++ src/components/Recipes/Recipes.tsx
@@ -0,0 +1,44 @@
+import './Recipes.css';
+import React, { useEffect, useState } from 'react';
+import EmptyState from "./EmptyState";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { fetchModalRecipesData } from "../../store/reducers/recipes/recipes-reducer";
+import List from "./List";
+import { selectRecipesFilteredList, selectRecipesList } from "../../store/reducers/recipes/recipes-selectors";
+import ModalContainerList from "../Modal/ModalContainer/ModalContainerList";
+
+const Recipes = () => {
+
+    const list = useAppSelector(selectRecipesList);
+    const filteredList = useAppSelector(selectRecipesFilteredList)
+    const dispatch = useAppDispatch();
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleAddClick = () => {
+        dispatch(fetchModalRecipesData);
+        setIsModalOpen(true);
+    }
+
+    useEffect(() => {
+        localStorage.setItem('list', JSON.stringify(list));
+    }, [list]);
+
+
+    return (
+        <div className='Recipes'>
+            {
+                filteredList.length > 0 ?
+                    <List {...{list: filteredList, handleAddClick}}/>
+                    :
+                    list.length > 0 ?
+                        <List {...{list, handleAddClick}}/> :
+                        <EmptyState {...{handleAddClick}}/>
+            }
+            <ModalContainerList {...{isModalOpen, setIsModalOpen}}/>
+        </div>
+    );
+};
+
+
+export default Recipes;
