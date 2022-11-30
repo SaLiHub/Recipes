3f73bce866b9f4cbd11d9d414e4075dccbe332c3
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2022-11-30 20:40:41 +0200

    Add create nutrition function

--- src/components/Modal/ModalContainer/ModalContainerInfo.tsx
+++ src/components/Modal/ModalContainer/ModalContainerInfo.tsx
@@ -1,19 +1,58 @@
 import React from 'react';
 import { useActions, useAppSelector } from "../../../hooks/redux";
-import { selectRecipesModalState } from "../../../store/reducers/recipes/recipes-selectors";
+import {
+    selectRecipesListItem,
+    selectRecipesModalState,
+    selectRecipesPaginationIndex
+} from "../../../store/reducers/recipes/recipes-selectors";
 import ModalContentInfo from "../ModalContent/ModalContentInfo";
 import Modal from "../Modal";
+import { Nutrition } from "../../../types";
+import Pagination from "../../Pagination/Pagination";
+
 
 const ModalContainerInfo = () => {
 
     const {closeItemModal} = useActions();
     const isModalOpen = useAppSelector(selectRecipesModalState);
+    const recipe = useAppSelector(selectRecipesListItem);
+    const index = useAppSelector(selectRecipesPaginationIndex);
+
+    let dish;
+    if (recipe.recipes === undefined) {
+        dish = recipe;
+    } else {
+        dish = recipe.recipes[index];
+
+    }
+
+
+    const createNutrition = (obj: Nutrition) => {
+        const nutritionArray = Object.entries(obj).filter(([key]) => key !== 'updated_at');
+        if (nutritionArray.length === 0) return '-';
+        return nutritionArray.map(([k, v]) =>
+            (
+                <div>
+                    <span>{k} </span>
+
+                    -
+
+                    <span> {v}</span>
+                </div>
+            )
+        )
+    }
 
     return (
-        <Modal {...{handleClosing: closeItemModal, isModalOpen}}>
-            <ModalContentInfo/>
+        <Modal {...{handleClosing: closeItemModal, isModalOpen,}}>
+            <ModalContentInfo {...{recipe: dish, createNutrition}}/>
+            {recipe.recipes !== undefined
+                ? <Pagination length={recipe.recipes.length}/>
+                : ''
+            }
         </Modal>
     );
 };
+
 
 export default ModalContainerInfo;
