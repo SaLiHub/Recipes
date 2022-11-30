e2537d0c9713fca1895471bf1e1aefc472c9f4f7
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2022-11-30 20:44:35 +0200

    Add new properties

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,31 @@
 export interface RecipeData {
-    id: string;
+    id: number;
     name?: string;
-    recipe?: string;
+    thumbnail_url?: string;
+    country?: string;
+    tags: [
+        {
+            name: string;
+            id: number;
+            display_name: string;
+        }
+    ];
+    description?: string;
+    nutrition: {};
+    user_ratings: {
+        score: number;
+        count_negative: number,
+        count_positive: number,
+    };
+    original_video_url: string;
+    instructions: [{
+        id: number,
+        display_text: string;
+        position: number;
+    }]
+    recipes?: RecipeData[];
+}
+
+export interface Nutrition {
+    [key: string]: number;
 }
